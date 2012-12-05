61675c8f758227cc680c0ace997221400bb8a928
Jason Jackson <jackson@twitter.com>
2012-12-05 01:53:45 -0800

    add repository

--- storm-kafka/project.clj
+++ storm-kafka/project.clj
@@ -1,9 +1,10 @@
-(defproject storm/storm-kafka "0.8.0-wip2-scala292"
+(defproject storm/storm-kafka "0.8.0-wip5-scala292"
   :source-path "src/clj"
   :java-source-path "src/jvm"
   :javac-options {:debug "true" :fork "true"}
-  :repositories {"releases" "http://artifactory.local.twitter.com/libs-releases-local"
-                 "snapshots" "http://artifactory.local.twitter.com/libs-snapshots-local"}
+  :repositories {;;"releases" "http://artifactory.local.twitter.com/libs-releases-local"
+                 ;;"snapshots" "http://artifactory.local.twitter.com/libs-snapshots-local"
+                 "conjars" "http://conjars.org/repo/"}
   :dependencies [[com.twitter/kafka_2.9.2 "0.7.0"
                   :exclusions [org.apache.zookeeper/zookeeper
                                log4j/log4j]]]
