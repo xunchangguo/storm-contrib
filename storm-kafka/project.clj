f1364154a9e7a72b513d2dd0e3430baf5cc272bb
Jason Jackson <jackson@twitter.com>
2012-12-05 01:59:41 -0800

    back to 0.9.0-wip3

--- storm-kafka/project.clj
+++ storm-kafka/project.clj
@@ -8,7 +8,7 @@
   :dependencies [[com.twitter/kafka_2.9.2 "0.7.0"
                   :exclusions [org.apache.zookeeper/zookeeper
                                log4j/log4j]]]
-  :dev-dependencies [[storm "0.8.2-wip19"]
+  :dev-dependencies [[storm "0.9.0-wip3"]
                      [org.slf4j/log4j-over-slf4j "1.6.6"]
                      [ch.qos.logback/logback-classic "1.0.6"]
                      [org.clojure/clojure "1.4.0"]]
